e7d3a33956fb039c247440d1fc5d306703da41a0
Julio Gomes Pena <juliogomespena@gmail.com>
2025-08-15 11:13:53 +0100

    feat: add migration for Money and Quantity value objects
    - Created `MoneyAndQuantityValueObjects` migration for database schema updates.
    - Added currency fields to `Item`, `ItemVariableCost`, `Sale`, and `SaleLine` tables.
    - Altered decimal fields to set precision, scale, and default values where applicable.
    - Updated `EdhDbContextModelSnapshot` to reflect migration changes.

---
+++ Infrastructure/EDH.Infrastructure.Data/Migrations/20250815095924_MoneyAndQuantityValueObjects.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EDH.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoneyAndQuantityValueObjects : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Adjustment",
+                table: "SaleLine",
+                type: "TEXT",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldPrecision: 18,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "SaleLine",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAdjustment",
+                table: "Sale",
+                type: "TEXT",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldPrecision: 18,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "Sale",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "ItemVariableCost",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "Item",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "SaleLine");
+
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Sale");
+
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "ItemVariableCost");
+
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Item");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Adjustment",
+                table: "SaleLine",
+                type: "TEXT",
+                precision: 18,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldPrecision: 18,
+                oldScale: 2,
+                oldDefaultValue: 0m);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAdjustment",
+                table: "Sale",
+                type: "TEXT",
+                precision: 18,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldPrecision: 18,
+                oldScale: 2,
+                oldDefaultValue: 0m);
+        }
+    }
+}
