9d8bb69ed08f4791927631e142c48cdd6da99a90
Julio Gomes Pena <juliogomespena@gmail.com>
2025-08-16 10:36:48 +0100

    revert: changes in code behind of edit stock quantities

--- Modules/EDH.Inventory/EDH.Inventory.Presentation/Resources/Dialogs/EditStockQuantitiesDialog.xaml.cs
+++ Modules/EDH.Inventory/EDH.Inventory.Presentation/Resources/Dialogs/EditStockQuantitiesDialog.xaml.cs
@@ -1,14 +1,28 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace EDH.Inventory.Presentation.Resources.Dialogs;
-
-/// <summary>
-/// Interaction logic for EditStockQuantitiesDialog.xaml
-/// </summary>
-public partial class EditStockQuantitiesDialog : UserControl
+namespace EDH.Inventory.Presentation.Resources.Dialogs
 {
-    public EditStockQuantitiesDialog()
+    /// <summary>
+    /// Interaction logic for EditStockQuantitiesDialog.xaml
+    /// </summary>
+    public partial class EditStockQuantitiesDialog : UserControl
     {
-        InitializeComponent();
+        public EditStockQuantitiesDialog()
+        {
+            InitializeComponent();
+        }
     }
 }
